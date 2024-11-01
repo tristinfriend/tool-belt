1a6ebbf2c299bddf8213a2360dd5cb93501dd6b5
tristinfriend <tristin.friend@maine.edu>
2024-11-01 09:18:09 -0400

    Update #1
    HW #6 and inclass example.

---
+++ Homework_6.py
@@ -0,0 +1,25 @@
+# File: tool-belt main.py
+# Author: Troy Schotter
+# Date: 11/1/2024
+
+# Function definition that's not the main function
+def countEvens(aArray):
+    print("Starting countEvens") # Placeholder to fill function
+
+def countLarger(numArray, x):
+    print("starting countLarger")
+
+def isPalindrome(aString):
+    print("Starting isPalindrome")
+
+def fibGenerator(fibSize):
+    print("starting fibGenerator")
+
+# Last function you should define is the main function
+def main():
+    print(countEvens([4,6,1,3,9,10]))
+    print(countEvens([4,6,100,0,9,10])
+
+
+# Call the main function at the global level
+main()
