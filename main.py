1c2a5072f3904d1ac0a5f66633f2086c876786f2
tristinfriend <tristin.friend@maine.edu>
2024-11-04 12:02:18 -0500

    Update HW #2
    First problem.

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ Professor Troy Shotter
 10/21/2024
 '''
 
+# countEvens
+def countEven (alist):
+    c = 0
+    for i in alist:
+        if i % 2 == 0:
+            c += 1
+    return c 
+
+x = countEven([2,3,4])
+print(x)
+
+
+    
